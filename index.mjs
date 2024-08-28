e76d0b54eaeed802b75518408fcefe37cfac66d3
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2024-08-28 18:25:22 +0200

    setup dockercompose

--- index.mjs
+++ index.mjs
@@ -66,7 +66,7 @@ app.get('*', (req, res) => {
 
 dbDebugger('Connected to the database...');
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
